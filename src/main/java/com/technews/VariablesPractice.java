632d584561d419a9621968090e101719e114d73a
Peter Jolley <peterjolley456@gmail.com>
2022-09-01 17:58:49 +0930

    testing with variables

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,15 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main (String[] args) {
+        int a = 4;
+        double b = 4.6;
+        double sum = a+b;
+        String c = "willow";
+        String concat = a+c;
+
+        System.out.println(sum);
+        System.out.println(concat);
+
+    }
+}
